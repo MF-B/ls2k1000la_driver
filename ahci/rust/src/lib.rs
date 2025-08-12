d750098216416d635414b6322ba39650560561be
mf1bzz <guzemin@hotmail.com>
2025-08-12 22:06:51 +0800

    tt

--- ahci/rust/src/lib.rs
+++ ahci/rust/src/lib.rs
@@ -7,10 +7,3 @@ pub mod libata;
 pub mod platform;
 
 use core::panic::PanicInfo;
-
-// Default panic handler for standalone usage
-// When used as a dependency, this can be overridden by the parent crate
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
