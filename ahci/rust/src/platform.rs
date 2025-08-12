908813f57346a62192b9d894716ab8c5cbba7d36
mf1bzz <guzemin@hotmail.com>
2025-08-12 23:04:38 +0800

    ffi

--- ahci/rust/src/platform.rs
+++ ahci/rust/src/platform.rs
@@ -1,6 +1,6 @@
 use core::arch::asm;
 
-/*
+
 // for C ffi test
 unsafe extern "C" {
     pub fn ahci_mdelay(ms: u32);
@@ -9,30 +9,30 @@ unsafe extern "C" {
     pub fn ahci_phys_to_uncached(va: u64) -> u64;
     pub fn ahci_virt_to_phys(va: u64) -> u64;
 }
-*/
 
-// 等待数毫秒
-pub fn ahci_mdelay(ms: u32) {}
 
-// 同步dcache中所有cached和uncached访存请求
-pub fn ahci_sync_dcache() {
-    unsafe {
-        asm!("dbar 0");
-    }
-}
+// // 等待数毫秒
+// pub fn ahci_mdelay(ms: u32) {}
 
-// 分配按align字节对齐的内存
-pub fn ahci_malloc_align(size: u64, align: u32) -> u64 {
-    0
-}
+// // 同步dcache中所有cached和uncached访存请求
+// pub fn ahci_sync_dcache() {
+//     unsafe {
+//         asm!("dbar 0");
+//     }
+// }
 
-// 物理地址转换为uncached虚拟地址
-pub fn ahci_phys_to_uncached(pa: u64) -> u64 {
-    pa
-}
+// // 分配按align字节对齐的内存
+// pub fn ahci_malloc_align(size: u64, align: u32) -> u64 {
+//     0
+// }
 
-// cached虚拟地址转换为物理地址
-// ahci dma可以接受64位的物理地址
-pub fn ahci_virt_to_phys(va: u64) -> u64 {
-    va
-}
+// // 物理地址转换为uncached虚拟地址
+// pub fn ahci_phys_to_uncached(pa: u64) -> u64 {
+//     pa
+// }
+
+// // cached虚拟地址转换为物理地址
+// // ahci dma可以接受64位的物理地址
+// pub fn ahci_virt_to_phys(va: u64) -> u64 {
+//     va
+// }
