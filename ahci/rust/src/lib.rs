26d6c48da042e4f9ec515bbc5087ec0cdf326b69
mf1bzz <guzemin@hotmail.com>
2025-08-12 19:27:08 +0800

    Convert to rlib and simplify panic handler

--- ahci/rust/src/lib.rs
+++ ahci/rust/src/lib.rs
@@ -6,14 +6,10 @@ pub mod libahci;
 pub mod libata;
 pub mod platform;
 
-// Panic handler is only included when building as a standalone library
-// When used as a dependency, the parent crate should define the panic handler
-#[cfg(not(test))]
-#[cfg(feature = "panic-handler")]
 use core::panic::PanicInfo;
 
-#[cfg(not(test))]
-#[cfg(feature = "panic-handler")]
+// Default panic handler for standalone usage
+// When used as a dependency, this can be overridden by the parent crate
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
